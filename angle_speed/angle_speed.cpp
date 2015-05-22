927b7fd212b65a917cbd214265cc0252be7f1fe3
Micke <miandre@kth.se>
2015-05-22 18:06:48 +0200

    Working code! Comments added!

--- angle_speed/angle_speed.cpp
+++ angle_speed/angle_speed.cpp
@@ -1,36 +1,75 @@
-// Do not remove the include below
+/********************HEADER******************************************
+ *
+ *   angle_speed.cpp
+ *
+ *   Created on: 22 apr 2015
+ *       Authors:
+ *       Ramón Rodriguez, Mikael André, Simon Johansson, Hannes Paulsson
+ *
+ *     S.A.R.A - Revamped
+ *
+ *     Main program to run an RC-car controlled by an
+ *     Android application.
+ *
+ *     This code is written to be run on an
+ *     Arduino UNO with the following outboard equipment:
+ *
+ *     - PKA03 MotorShield
+ *     - HC-06 Bluetooth transceiver
+ *     - HC-SR04 Ultrasoniv distance sensor
+ *     - 5V DC-Servo
+ *     - DC engine
+ *
+ *
+ **************************************************************/
+
+/*************INCLUDES*************/
 #include "angle_speed.h"
 #include "SoftwareServo.h"
 #include "NewPing.h"
 
-
-//#include "parser.h"
+/************DEFINES*************/
 #define servoPin 11
 #define pwmPin 3
 #define dirPin 2
 #define trigPin 5
 #define echoPin 6
 #define honkPin 12
-#define vSensPin A5
+#define vSensPin A4
+#define ledPin A0
 
-
+/****************PROTOTYPES************/
 void parser(byte[], int);
 void servoAttatch(void);
 void servoRefresh(void);
 void breakPulse(void);
 int setAngle(uint8_t angle);
 int sensor(void);
-int timer;
-int recievedIndex = 0;
-int distance = 100;
-//NewPing sonar(trigPin, echoPin, 200);
-byte input[2];
 
+/************GLOBAL VARIABLES*************/
+float output;
+int timer =0;
+int recievedIndex = 0;
+int distance = 500;
+byte input[3];
 boolean hasBreaked = false;
 boolean started = false;
 
 
-//The setup function is called once at startup of the sketch
+/************************************************
+ * Arduino setup function
+ *
+ * Initiates:
+ *  - input/output pins
+ *  - servo
+ *  - serial communication
+ *
+ *  LED on car is set to off.
+ *
+ *  Baud rate for serial communication is
+ *  set to 9600 (default for Bluetooth device HC-06)
+ *
+ ***********************************************/
 void setup()
 {
 	pinMode(servoPin,OUTPUT);
@@ -40,69 +79,79 @@ void setup()
 	pinMode(echoPin, INPUT);
 	pinMode(honkPin,OUTPUT);
 	pinMode(vSensPin,INPUT);
-	pinMode(A3, OUTPUT);
+	pinMode(ledPin, OUTPUT);
 	Serial.begin(9600);
 	servoAttatch();
-	analogWrite(A3, 0);
-	timer = 0;
+	digitalWrite(A3, LOW);
 
-
-// Add your initialization code here
 }
 
-// The loop function is called in an endless loop
+/*****************************************************************************
+ * Main loop.
+ * Not much is actually done here.
+ *
+ * The servoRefresh function is called (required by SoftwareServo library)
+ *
+ * Battey voltage level is read and sent to serial port.
+ * Voltage level sent via BlueTooth to Android Application.
+ * Signs to help parser:
+ * # = start sign
+ * @ = end sign
+ *
+ ****************************************************************************/
 void loop()
 {
-	servoRefresh();
+	servoRefresh();  //SoftwareServo function that must be called at leas every 20ms
 
 
+	timer++;
+	if(timer == 10000){
+		output=(float) ((analogRead(vSensPin)*5.0/1023.0));
 
-	//Serial.println(distance);
-
-	/*
-	byte input[2];
-	float output;
-	if(Serial.available()>0){
-		Serial.readBytes(input,2);
-		parser(input);
-
-	}else{
-
-*/
-//	timer++;
-//	if(timer == 100000){
-//		output=(float) ((analogRead(vSensPin)*5.0/1023.0));
-//
-//		Serial.print('#');
-//		Serial.print(output);
-//		Serial.print('@');
-////		timer = 0;
-//		}
-
+		Serial.print('#');
+		Serial.print(output);
+		Serial.print('@');
+		timer = 0;
 	}
 
+}
+
+
+
+/*********************************************************
+ * serialEvent() is executed after each loop(), if there is serial data available.
+ *
+ * Serial data is read and used as argument to parser.
+ * Incoming serial data will be on the format:
+ * {[253],[key],[value][254]}
+ * 253 = startsign
+ * 254 = stopsign
+ *
+ *********************************************************/
 void serialEvent(){
 
-	//distance = sonar.ping_cm();
-	//if(distance <5) distance = 100;
 	if(Serial.available()){
 		byte in = Serial.read();
 
-		if(in == 254){ //254 = stoptecken
+		if(in == 254){
 			parser(input, distance);
 			input[0] = 0;
 			input[1] = 0;
+			input[2] = 0;
 			started = 0;
 			recievedIndex = 0;
+
 		}else if(started){
 			input[recievedIndex] = in;
 			recievedIndex++;
-		}else if(in == 253){ //253 = starttecken
+
+		}else if(in == 253){
 			started = true;
+
 		}
 	}
 }
-//Add your repeated code here
+
 
 
 
--- angle_speed/parser.cpp
+++ angle_speed/parser.cpp
@@ -1,36 +1,60 @@
-/*
+/****************************************************************
  * parser.cpp
  *
  *  Created on: 22 apr 2015
- *      Author: Rodriguez
- */
+ *      Authors:
+ *      Ramón Rodriguez, Mikael André, Simon Johansson, Hannes Paulsson
+ *
+ *      This file contains the main functions to interpret and handle
+ *      incoming serial data.
+ *
+ ****************************************************************/
+
+/*************INCLUDES*************/
 #include <stdbool.h>
 #include <stdint.h>
 #include <arduino.h>
 #include "SoftwareServo.h"
 #include "NewPing.h"
 
+/************DEFINES*************/
 #define trigPin 5
 #define echoPin 6
 #define forward true
 #define reverse false
 
+
+/****************PROTOTYPES************/
 void setSpeed(uint8_t,bool);
 int setAngle(uint8_t);
 void breakPulse();
 void parser(byte[], int);
 void servoAttatch(void);
-SoftwareServo servo;
+int getAvarageDistance(int inDist);
+
+
+/************GLOBAL VARIABLES*************/
 static bool fwd=true;
-static bool stop = false;
+static bool useStopper = true;
 static bool dir;
 static bool lastDir=reverse;
 static bool first = true;
 extern bool hasBreaked;
-NewPing sonar(trigPin, echoPin, 200);
 int medianDistance[5];
 uint32_t medianDistanceIndex = 0;
 
+
+/************INITIATIONS********************/
+NewPing sonar(trigPin, echoPin, 200);
+SoftwareServo servo;
+
+
+
+/*********************************************************
+ * Break pulse is called by the automatic stop function
+ * as long as the car is too close to the object in front
+ * and the user is trying to drive forward
+ ********************************************************/
 void breakPulse(){
 	setSpeed(255,false);
 	delay(200);
@@ -38,30 +62,29 @@ void breakPulse(){
 	hasBreaked=true;
 }
 
+
+/*****************************************************************
+ * The parser function is used to interpret incoming serial data
+ * and act accordingly.
+ * The input data will be on the form {[key],[value]}, where the key
+ * defines the action to be performed.
+ *
+ * Keys:
+ * 200 --->  Steering   - Value: angle
+ * 201 --->  Engine     - Value: speed
+ * 202 --->  DistSensor - Value : 100=off 101=on
+ *
+ *
+ ********************************************************************/
+
 void parser(byte input[], int distance){
 
 
 	uint8_t op=input[0];
 	uint8_t ammount=input[1];
 	bool dir=false;
-	int tempDist = sonar.ping_cm();
-	if(tempDist > 4)
-	{
-		medianDistance[medianDistanceIndex % 5] = tempDist;
-		medianDistanceIndex++;
-		distance = 0;
-		for(int i = 0; i < 5; i++) distance += medianDistance[i];
 
-		distance = distance / 5;
-		if(medianDistanceIndex<5) distance = 100;
-	}
-	else distance = 100;
-	//	if(distance < 40 && !hasBreaked){
-	//		breakPulse();
-	//		return;
-	//	}
-
-	Serial.println(distance);
+	distance = getAvarageDistance(distance); //Get average distance to avoid missreadings
 
 	switch(op){
 	case 200:
@@ -69,10 +92,17 @@ void parser(byte input[], int distance){
 		servo.write(ammount);
 		break;
 
+
 	case 201:
 		if(ammount>50){
-			if(distance<40 && distance > 5){
-				breakPulse();
+			if(useStopper){
+				if(distance<40 && distance > 5){
+					breakPulse();
+				}
+				else{
+					dir=true;
+					ammount=((ammount-50)*5.1);
+					setSpeed(ammount,dir);}
 			}else{
 				dir=true;
 				ammount=((ammount-50)*5.1);
@@ -88,36 +118,61 @@ void parser(byte input[], int distance){
 			setSpeed(ammount,dir);
 		}
 		break;
+
+
+	case 202:
+		if(ammount==101){
+			useStopper=false;
+			digitalWrite(ledPin,HIGH);
+		}else if (ammount == 100){
+			useStopper=true;
+			digitalWrite(ledPin,LOW);
+		}
 	}
 
-	//
-	//		if(ammount>50 && !hasBreaked){
-	//			dir=true;
-	//			ammount=((ammount-50)*5.1);
-	//			setSpeed(ammount,dir);
-	//		}
-	//		else if(ammount==50){
-	//			ammount=0;
-	//			setSpeed(ammount,dir);
-	//		}
-	//		else if(ammount<50){
-	//			dir=false;
-	//			hasBreaked=false;
-	//			ammount=abs((ammount-50)*5.1);
-	//			setSpeed(ammount,dir);
-	//		}
-	//
-	//		break;
-	//	}
-	//
+
 	return;
 }
 
+/*****************************************
+ * Initiate servo and set wheels straight
+ ****************************************/
 void servoAttatch(){
 	servo.attach(11);
 	servo.write(90);
 }
 
+
+/*********************************************
+ * SoftwareServo function called from mainloop
+ ********************************************/
 void servoRefresh(){
 	SoftwareServo::refresh();
+}
+
+
+/******************************************************************
+ * This function calculates the average distance based
+ * on the last 5 inputs.
+ *
+ *  @return  distance in cm
+ *****************************************************************/
+int getAvarageDistance(int inDist){
+
+	int tempDist = sonar.ping_cm();
+
+		if(tempDist > 4)
+		{
+			medianDistance[medianDistanceIndex % 5] = tempDist;
+			medianDistanceIndex++;
+			inDist = 0;
+			for(int i = 0; i < 5; i++) inDist += medianDistance[i];
+
+			inDist = inDist / 5;
+			if(medianDistanceIndex<5) inDist = 100; //To avoid bad values when the first 5 inputs are empty.
+			return inDist;
+		}
+
+		else return 100;
+
 }
